5d51583de9cd88008dee247ea8a217da1b623f80
liorb <liorb@gettaxi.com>
2016-01-20 12:01:30 +0200

    create firebase template (index.html = testing)

--- firebase_sdk.js
+++ firebase_sdk.js
@@ -1,3 +1,35 @@
-/**
- * Created by liorb on 1/20/16.
- */
+
+var launches = new Firebase("https://gonzo1234.firebaseio.com/launches");
+
+
+function addEvent(name, lat, long, time){
+    launches.set({
+        "name": name,
+        "lat": lat,
+        "long": long,
+        'time': Math.random(), // "value" event will not trigger unless some value changes
+    })
+}
+
+function onEventsChanged(cb) {
+    launches.child("/").on("value", cb);
+    console.log("event registered")
+}
+
+
+onEventsChanged(function(snapshot){
+    console.log("event triggered");
+    $("body").append("<div>" + snapshot.val().name + "</div>");
+    $("body").append("<br>");
+});
+
+$(function(){
+    addEvent("Shiskabbab", 0, 0, 19);
+});
+
+
+
+
+
+
+
